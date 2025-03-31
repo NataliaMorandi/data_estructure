f4a85f9d5bfec1883a8d95824b96bce626925cd8
nataliamorandi <nataliamorandirm@gmail.com>
2025-03-30 22:38:27 -0300

    busca sequencial e busca binaria

--- buscabinaria.py
+++ buscabinaria.py
@@ -5,26 +5,45 @@ class Lista:
         self.numeroEscolhido = numeroEscolhido
         self.inicio = 0
         self.fim = len(self.lista) -1 
-        self.count = 0
+
+
+    def buscaSequencial(self):
+        count = 0
+        for indice, values in enumerate(self.lista):
+            count += 1
+            if values == self.numeroEscolhido:
+                print(f"Achei o {self.numeroEscolhido} na posicao {indice} em {count} operacoes sequenciais")
+                return indice
+        print("Numero nao encontrado na busca sequencial!")
+        return -1
+            
 
 
     def buscaBinaria(self):
-        #numeroEscolhido = int(input("Digite o número que deseja buscar: "))
+        self.inicio = 0
+        self.fim = len(self.lista) -1
+        count = 0 
         while self.inicio <= self.fim:
             meio = (self.inicio + self.fim) // 2
-            self.count += 1  
+            count +=1
 
             if self.lista[meio] == self.numeroEscolhido:
-                print(f"Achei o {self.numeroEscolhido} na posicao {meio} em {self.count} operacoes.")
+                print(f"Achei o {self.numeroEscolhido} na posicao {meio} em {count} operacoes binarias.") 
                 return meio
             elif self.lista[meio] < self.numeroEscolhido:
                 self.inicio = meio + 1  
             else:
                 self.fim = meio - 1  
             
-        print("Número não encontrado!")
+        print("Numero nao encontrado na busca binaria!")
         return -1  
 
-l1 = Lista(0, 50, 47)
+l1 = Lista(4, 55, 48)
 
+l1.buscaSequencial()
 l1.buscaBinaria()
+#l1.buscaBinaria()
+
+l2 = Lista(0, 10, 9)
+l2.buscaSequencial()
+l2.buscaBinaria()
---
+++ teste01.py
@@ -0,0 +1,7 @@
+lista = ['Maria', 'Helena', 'Luiz']
+lista.append('Joao')
+
+indices = range(len(lista))
+
+for indice in range(len(lista)):
+    print(indice, lista[indice])
